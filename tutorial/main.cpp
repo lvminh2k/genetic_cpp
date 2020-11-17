d5b68439c58dc0a4091989956cb3dda708d27320
Van-Minh Le <lvminh2k@gmail.com>
2020-11-17 14:16:12 +0700

    add operator >> and <<

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "Figures.h"
+#include "FigIO.h"
 
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+void inputCircleData(istream& inDevice, Circle &cir)
+{
+	double Xo, Yo, r;
+	inDevice >> Xo >> Yo >> r;
+	cir.Set(Xo, Yo, r);
+}
+
+void outputCircleData(ostream& outDevice, Circle &cir)
+{
+	outDevice << " + Area of circle = " << cir.Area() << endl;
+	outDevice << " + Perimeter of circle = " << cir.Perimeter() << endl;
+}
+
+//create Point2D and Circle objects
+//
+void testCase1()
+{
 	Point2D P;
 	P.Set(7.2, 4.3);
 	P.Move(2.7, 4.1);
@@ -16,5 +33,22 @@ int main(int argc, char** argv) {
 	
 	cout << "Area: " << area << endl;
 	cout << "Perimeter: " << perimeter << endl;
+}
+
+//input to object, output data to screen
+//
+void testcase2(){
+	Circle myCircle;
+	cout << "Input center and radius" << endl;
+	inputCircleData(cin, myCircle);
+	outputCircleData(cout, myCircle);
+}
+
+int main(int argc, char** argv) {
+	Circle myCircle;
+	cout << "Input center and radius: " << endl;
+	cin >> myCircle;
+	cout << myCircle;
+	cin.get();
 	return 0;
 }

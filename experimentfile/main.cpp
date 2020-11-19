78785cc99672e1785a2674975fb5b4dc56f9e71a
Van-Minh Le <lvminh2k@gmail.com>
2020-11-19 12:00:24 +0700

    add read write file methods

--- experimentfile/main.cpp
+++ experimentfile/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 void write_string_to_txt_file(){
@@ -20,9 +21,59 @@ void read_all_string_from_txt_file(){
     }
     inFile.close();
 }
+void write_int_to_file_then_read_it(){
+    ofstream outFile("data.txt", ios::out);
+    int i = 19;
+    outFile << i;
+    outFile.close();
+
+    ifstream inFile("data.txt");
+    int x;
+    inFile >> x;
+    cout << x;
+    inFile.close();
+}
+
+
+istream& operator >> (istream& inDev, vector<float>& a){
+	float temp;
+	a.resize(0);
+	while (inDev >> temp){
+		a.push_back(temp);
+	}
+	return inDev;
+}
+
+void input_vector_write_to_file_then_read_it(){
+    vector<float> a;
+	cout << "Input: ";
+	cin >> a;
+    cout << "display vector: ";
+    for (int i:a){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    ofstream outFile("data.txt", ios::out);
+    for (int i:a){
+        outFile << i << " ";
+    }
+    outFile.close();
+    cout << "finish writing to file" << endl;
+
+    cout << "read from file and display: ";
+    ifstream inFile("data.txt");
+    int x;
+    while (inFile >> x){
+        cout << x << " ";
+    }
+    inFile.close();
+}
 int main(){
-    write_string_to_txt_file();
-    read_all_string_from_txt_file();
+    //write_string_to_txt_file();
+    //read_all_string_from_txt_file();
+    //write_int_to_file_then_read_it();
+    input_vector_write_to_file_then_read_it();
     cout << "done";
     return 0;
 }
